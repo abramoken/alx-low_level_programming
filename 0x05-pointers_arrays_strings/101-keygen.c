deb6e176b75c3e75d746f45068eb5cfb7f4d95b0
abramoken <abioken33@gmail.com>
2022-03-21 22:44:56 +0300

    a program that generates random valid passwords for the program 101-crackme.

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -12,7 +13,7 @@ int main(void)
 {
 	int x;
 
-	srand((time(0)));
+	srand(time(0));
 
 	x = rand();
 	printf("%i\n", x);
