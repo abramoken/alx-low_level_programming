fccabf89ba04fe37be7f28d73ad10ff0f1c5cead
abramoken <abioken33@gmail.com>
2022-03-30 13:31:27 +0300

    a function that prints a string in reverse.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _puts_recursion - a function that prints a string, followed by a new line.
- * @s: An input string to printing
+ * @s: An input string to print
  * Return: Nothing
  */
 void _puts_recursion(char *s)
