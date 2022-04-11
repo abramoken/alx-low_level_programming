f0cd70d86a7bbc1da9e5718c4c7da9324fa150c5
abramoken <abioken33@gmail.com>
2022-04-12 00:30:04 +0300

    a function that frees dogs.

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,38 +10,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	char *cpyname, *cpyowner;
-	int len_name = 0, len_owner = 0, i;
+	int len_name, owner_len, i;
+	dog_t *d1;
 
-	if (name == NULL || owner == NULL)
+	d1 = (dog_t *)malloc(sizeof(dog_t));
+	if (d1 == NULL)
 		return (NULL);
-
-	while (name[len_name])
-		len_name++;
-	while (owner[len_owner])
-		len_owner++;
-
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == NULL)
+	len_name = owner_len = 0;
+	while (name[len_name++])
+		;
+	while (owner[owner_len++])
+		;
+	d1->name = malloc(len_name * sizeof(d1->name));
+	if (d1->name == NULL)
+	{
+		free(d1);
 		return (NULL);
-
-	cpyname = malloc(len_name + 1);
-	if (cpyname == NULL)
+	}
+	for (i = 0; i <= len_name; i++)
+		d1->name[i] = name[i];
+	d1->age = age;
+	d1->owner = malloc(owner_len * sizeof(d1->owner));
+	if (d1->owner == NULL)
+	{
+		free(d1->name);
+		free(d1);
 		return (NULL);
-	for (i = 0; name[i]; i++)
-		cpyname[i] = name[i];
-	cpyname[i] = '\0';
-
-	cpyowner = malloc(len_owner + 1);
-	if (cpyowner == NULL)
-		return (NULL);
-	for (i = 0; owner[i]; i++)
-		cpyowner[i] = owner[i];
-	cpyowner[i] = '\0';
-
-	new_dog->name = cpyname;
-	new_dog->age = age;
-	new_dog->owner = cpyowner;
-	return (new_dog);
+	}
+	for (i = 0; i <= owner_len; i++)
+		d1->owner[i] = owner[i];
+	return (d1);
 }
