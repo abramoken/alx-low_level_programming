ad9f4ebbf382060d62d06d77fd54de924d9efa85
abramoken <abioken33@gmail.com>
2022-04-26 22:02:18 +0300

    a function that prints the elements in a list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint_safe - a function that prints the elements in a list
+ * @head: a pointer to listint_t structure
+ * Return: number of nodes and exits with 98 on failure
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes = 0;
+	const listint_t *one = head, *two = head;
+
+	if (head == NULL)
+		exit(98);
+
+	while (one && two && two->next && head)
+	{
+		one = one->next;
+		two = two->next->next;
+		if (one == two)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			exit(98);
+		}
+
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nodes++;
+	}
+	head = NULL;
+	return (nodes);
+}
