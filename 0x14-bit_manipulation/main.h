e86642597551b059d6ecaf631753033b5c277bee
abramoken <abioken33@gmail.com>
2022-04-28 19:12:15 +0300

    a function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/*
+ * File: main.h
+ * AUth: Abraham Opiko
+ * Desc: Header file containing prototypes for all functions
+ * in the 0x14-bit_manipulation directory.
+ */
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif /* _MAIN_H_ */
