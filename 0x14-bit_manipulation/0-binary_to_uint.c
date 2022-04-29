02f24fe8d853620d4bdc6fa71bdff46c99561184
abramoken <abioken33@gmail.com>
2022-04-29 03:41:35 +0300

    modified the file binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - a function that converts a binary number to an unsigned int
@@ -11,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, mult = 1;
 	int len;
 
-	if (*b == '\0')
+	if (b == NULL)
 		return (0);
 
 	for (len = 0; b[len];)
